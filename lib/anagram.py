0a870e69cdebdcad6d89ce2d332514c0a01116e7
Patty Karigi Kariuki <pattykarigi96@gmail.com>
2025-02-28 23:33:21 +0300

    Initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+        self.matching_word = []
+
+    def match(self,my_list):
+        new_word = sorted(self.word)
+        for item in my_list:
+            new_item = sorted(item)
+            if new_word == new_item:
+                self.matching_word.append(item)
+        return self.matching_word
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
+
